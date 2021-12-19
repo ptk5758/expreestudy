636e67e8f01f306ec08a47dfd63c9999a15e2023
ptk5758 <76796879+ptk5758@users.noreply.github.com>
2021-12-19 22:42:41 +0900

    21-12-19 Get Api

--- main.js
+++ main.js
@@ -8,7 +8,8 @@ const conn = mysql.createConnection({     //데이터베이스 연결 객체
     database: 'attend'
 });
 
-app.use(express.json());
+app.use(express.json()); //body-parser 의역할? json request 받을때 필요함
+conn.connect(); // 데이터베이스 커넥션
 
 app.get("/", (req,res) => {
     res.type('application/json');
@@ -36,16 +37,26 @@ app.post("/attend", (req,res)=> {
     let name = req.body.name;
     let level = req.body.level;
 
-    conn.connect();
     conn.query(`INSERT INTO user (id,pass,name,level) VALUES ("${id}","${pass}","${name}",${level})`, (err, rows ,field)=>{        
         if(err) {
-            console.log(err);
+            throw err;
         }
         data = {};  // 빈값으로 초기화
         data["msg"] = "Good" //data 에 추가 추가 가능
         data["code"] = 123; // => 
         res.send(data);
     });    
+});
+
+app.get("/attend/:id", (req,res)=>{
+    let id = req.params.id;
+    let data = {};
+    data['msg'] = "Good";
+    conn.query(`SELECT * FROM user WHERE id='${id}' limit 1`, (err,rows,field)=>{
+        data['data'] = rows[0];
+        res.send(data);
+    });
+    
 });
 
 app.listen(5000, () => {console.log("5000번포트로 서버활성화");});
